33176c16ec30f654b546572f1b262f548cb03309
jordisan RamSys <ramsys@gmail.com>
2020-06-07 17:35:04 +0200

    Move base.html to /general

--- web_project/settings.py
+++ web_project/settings.py
@@ -58,7 +58,10 @@ ROOT_URLCONF = 'web_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'contactForm/templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'general/templates'),
+            os.path.join(BASE_DIR, 'contactForm/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
