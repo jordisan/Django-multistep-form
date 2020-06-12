685eec4296b14fd20142389d62ff4ae6a6d30fbd
jordisan RamSys <ramsys@gmail.com>
2020-06-12 18:49:51 +0200

    Add django-heroku

--- web_project/settings.py
+++ web_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +143,6 @@ STATICFILES_FINDERS = (
 COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
