3dee2f00c5a672e89ffb4f23c0f1214cf8390755
jordisan RamSys <ramsys@gmail.com>
2020-06-13 18:55:47 +0200

    Add models to admin

--- contactForm/admin.py
+++ contactForm/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from general.models.customer import Customer
+from .models.contactMessage import ContactMessage
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(ContactMessage)
