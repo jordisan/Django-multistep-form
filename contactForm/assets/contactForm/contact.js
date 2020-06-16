edc01aa537b4cba8946a57f37dcf7f617d7612ff
jordisan RamSys <ramsys@gmail.com>
2020-06-16 20:55:30 +0200

    Add JS modules for API and contact form; fulfill custom data calling the API

--- contactForm/assets/contactForm/contact.js
+++ contactForm/assets/contactForm/contact.js
@@ -1,31 +1,45 @@
+// global namespace
+var general = general || {};
+
 // module for contact form
-var contactForm = (function(){
-    var container = null;
+general.ContactForm = (function(){
+    'use strict';
 
-    function getCustomerData(email) {
-        var me = this;
-        if (!!email) {
-            fetch('/api/customers/' + encodeURIComponent(email))
-            .then(function(response) {
-                console.log(response);
-            })
-            .catch(function(err) {
-                console.log(err);
-            })
-        }
-    };
+    var _container = null;
 
-    function init(container) {
-        this.container = container;
-        if (!!container) {
+    /**
+     * Update form data from API using email
+     * TODO: obvious privacy issues here
+     * @param {*} email 
+     */
+    function _updateCustomerData(email) {
+        general.API.getCustomerData(email).then(function(data) {
+            if (!!data) {
+                _container.querySelector('#id_first_name').value=data.first_name;
+                _container.querySelector('#id_last_name').value=data.last_name;
+                _container.querySelector('#id_phone_number').value=data.phone_number;
+            }
+        });
+    }
+
+    /**
+     * Initialize contact form
+     * @param {*} container 
+     */
+    function _init(container) {
+        _container = container;
+        if (!! _container) {
             // attach listener to email field to get data from API
-            container.querySelector('#id_email').addEventListener('blur', function(e) {
-                getCustomerData(e.target.value);
-            });
+            var email_field = _container.querySelector('#id_email');
+            if (!! email_field) {
+                email_field.addEventListener('blur', function(e) {
+                    _updateCustomerData(e.target.value);
+                });
+            }
         }
     };
 
     return {
-        init: init
+        init: _init
     };
 })();
